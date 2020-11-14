0df6b40f6d70531c8a5613403368a63095125069
ayu7 <ayyu@umich.edu>
2020-11-13 20:51:57 -0500

    adding coin and making things disappear offscreen

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,6 +5,7 @@ const OBSTACLE_GAP = 150;
 let start_x = window.innerWidth - 80;
 let chimney_pic = "../../images/chimney_temp.png";
 let icicle_pic = "../../images/ice_temp.png";
+let coin_pic = "../../images/coin_temp.png"
 let movement_interval_handle;
 let obstacle_idx = 0;
 
@@ -28,15 +29,20 @@ function get_random_height(){
 function create_obstacle(){
     let top_height = get_random_height();
     let bottom_height = top_height + OBSTACLE_HEIGHT + OBSTACLE_GAP; 
+    let middle_height = top_height + OBSTACLE_HEIGHT + (OBSTACLE_GAP / 2) - 30; // 30 is half of coin size
 
-    let chimney_string = "<div id='c-" + obstacle_idx + "' style='position: absolute; left:" 
-                        + start_x + "px; top:" + bottom_height + "px' class='chimney'><img class='chimney' src='" 
-                        + chimney_pic + "'>";
     let icicle_string = "<div id='i-" + obstacle_idx + "' style='position: absolute; left:" 
                         + start_x + "px; top:" + top_height + "px' class='icicle'><img class='icicle' src='" 
                         + icicle_pic + "'>";
-    $(chimney_string).appendTo("body");
+    let coin_string = "<div id='co-" + obstacle_idx + "' style='position: absolute; left:" 
+                        + (start_x + 10) + "px; top:" + middle_height + "px' class='coin'><img class='coin' src='" 
+                        + coin_pic + "'>";
+    let chimney_string = "<div id='c-" + obstacle_idx + "' style='position: absolute; left:" 
+                        + start_x + "px; top:" + bottom_height + "px' class='chimney'><img class='chimney' src='" 
+                        + chimney_pic + "'>";
     $(icicle_string).appendTo("body");
+    $(coin_string).appendTo("body");
+    $(chimney_string).appendTo("body");
     move_obstacles(obstacle_idx);
     obstacle_idx++;
 };
@@ -44,17 +50,20 @@ function create_obstacle(){
 function move_obstacles(id){
     let xChange = 2;
     let iterationsLeft = window.innerWidth / xChange;
-    let chimneyObj = $("#c-" + id);
     let icicleObj = $("#i-" + id);
+    let coinObj = $("#co-" + id);
+    let chimneyObj = $("#c-" + id);
     for (let i = 0; i < iterationsLeft; i++){
         setTimeout(function(){ 
             let newXPos = parseInt(chimneyObj.css("left")) - xChange;
-            if (newXPos < 10){
-                chimneyObj.remove();
+            if (newXPos < -100){
                 icicleObj.remove();
+                coinObj.remove();
+                chimneyObj.remove();
             }
-            chimneyObj.css('left', newXPos); 
             icicleObj.css('left', newXPos); 
+            coinObj.css('left', newXPos + 10); 
+            chimneyObj.css('left', newXPos); 
             // console.log("moving item");
         }, i * 10);
     }
