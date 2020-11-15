189803e8567134a938ffad2932b674950d47760f
David Hu <daweihu@umich.edu>
2020-11-14 21:21:16 -0500

    backend first draft

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,31 @@
 from flask import Flask, render_template, request
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return render_template('leaderboard.html')
+    
+    return render_template('submit.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':       
+        username = request.form['username']
+        score = 200 # hardcoded for now 
+        if username == '': 
+            return render_template('submit.html',  message="Whoops,please enter a valid username!")
+
+        with open('leaderboard.txt') as f:
+            score_list = f.read().splitlines()
+        score_list.pop() 
+
+        score_list.append(username + ' ' + str(score))
+        score_list = sorted(score_list, key=lambda s:int(s.split()[1]), reverse=True)
+
+        with open('leaderboard.txt', 'w') as writer:
+            for score in score_list:
+                writer.write(score + '\n')
+        return render_template('leaderboard.html', score_list=score_list)
 
 if __name__ == '__main__':
     app.debug = True
