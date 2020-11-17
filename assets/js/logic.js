9f2e46a8cbd6377f9be3915ffd4e3ce2fd082527
Jonathan Yun <johnnyun@umich.edu>
2020-11-17 15:56:37 -0800

    My initial changes

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,22 +1,39 @@
 const GENERATE_SPEED = 3000;
 const OBSTACLE_HEIGHT = 400;
 const OBSTACLE_GAP = 150;
+const KEYS = {
+    up: 38,
+    down: 40,
+    spacebar: 32
+};
 
 let start_x = window.innerWidth - 80;
 let chimney_pic = "../../images/chimney.svg";
 let icicle_pic = "../../images/icicle.svg";
-let coin_pic = "../../images/coin.svg"
+let coin_pic = "../../images/coin.svg";
+let present_pic = "../../images/present_temp.svg";
 let movement_interval_handle;
+let gravity_interval_handle;
 let obstacle_idx = 0;
+let present_idx = 0;
+let player;
 
 $(document).ready( function() {
+    player = $('#player');  // set the global player handle
+    $(window).keydown(keydownRouter);
     console.log("begin game")
     start_game();
 });
 
 function start_game(){
     movement_interval_handle = setInterval(create_obstacle, GENERATE_SPEED);
-    // create_obstacle()
+    gravity_interval_handle = setInterval(function() {
+        let newPos = parseInt(player.css('top'))+15;
+        // if (newPos > maxPersonPosY) {
+        //     newPos = maxPersonPosY;
+        // }
+        player.css('top', newPos);
+    }, 100);
 };
 
 function get_random_height(){
@@ -47,6 +64,13 @@ function create_obstacle(){
     obstacle_idx++;
 };
 
+function create_present(upwards) {
+    let present_string = "<div id='p-" + present_idx + "' class='present'><img class='present' src='" + present_pic + "'>";
+    $(present_string).appendTo("body");
+    move_presents(present_idx);
+    present_idx++;
+};
+
 function move_obstacles(id){
     let xChange = 2;
     let iterationsLeft = window.innerWidth / xChange;
@@ -69,7 +93,48 @@ function move_obstacles(id){
     }
 }
 
+function move_presents(id){
+    
+}
+
 function increase_Score(){
     let score = parseInt($("#score").text().slice(7)) + 1;
     $("#score").text("Score: " + score);
+}
+
+function keydownRouter(e) {
+    switch (e.which) {
+        case KEYS.spacebar:
+        case KEYS.up:
+        case KEYS.down:
+            movePerson(e.which);
+            break;
+        default:
+            console.log("Invalid input!");
+    }
+}
+
+// Handle player movement events
+function movePerson(arrow) {
+    switch (arrow) {
+        case KEYS.up: { // throw present up
+            console.log("Up!");
+            create_present(true);
+            break;
+        }
+        case KEYS.down: { // throw present down
+            console.log("Down!");
+            create_present(false);
+            break;
+        }
+        case KEYS.spacebar: { // fly up once
+            console.log("Spacebar!");
+            let newPos = parseInt(player.css('top'))-25;
+            if (newPos < 0) {
+                newPos = 0;
+            }
+            player.css('top', newPos);
+            break;
+        }
+    }
 }
